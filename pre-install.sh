0578c0737a79f2aa82e701208a47330058aeae5b
jmaver <jm18457@gmail.com>
2022-06-05 19:08:42 +0200

    Remove else condition in pre-install oh-my-zsh

--- pre-install.sh
+++ pre-install.sh
@@ -4,5 +4,4 @@ OH_MY_ZSH_DIR="$HOME/.oh-my-zsh"
 if [ ! -d "$OH_MY_ZSH_DIR" ]; then
     echo 'Installing oh-my-zsh'
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-else
 fi
