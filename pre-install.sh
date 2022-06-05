9e2b6807c04f2ead6cf88395e70cd08347c6f332
jmaver <jm18457@gmail.com>
2022-06-05 19:07:45 +0200

    Pre-install condition

--- pre-install.sh
+++ pre-install.sh
@@ -5,7 +5,4 @@ if [ ! -d "$OH_MY_ZSH_DIR" ]; then
     echo 'Installing oh-my-zsh'
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 else
-    echo 'oh-my-zsh is already installed'
-    echo 'Updating oh-my-zsh'
-    upgrade_oh_my_zsh
 fi
