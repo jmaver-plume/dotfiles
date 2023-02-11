a7baaa986a45c322a16e032ebf7f96fbde26f7d3
Jaka Maver <jmaver@plume.com>
2023-02-11 09:45:00 +0100

    feat: Add commit functions

--- oh-my-zsh/custom/my_aliases.zsh
+++ oh-my-zsh/custom/my_aliases.zsh
@@ -2,3 +2,11 @@ alias dotfiles='cd ~/Documents/code/.dotfiles'
 alias hgrep='history | grep'
 alias personal='cd ~/Documents/code/personal'
 alias iso_date='date -u +%Y-%m-%dT%H:%M:%SZ'
+
+function gc {
+  git commit -m $1
+}
+
+function gca {
+  git commit -a -m $1
+}
