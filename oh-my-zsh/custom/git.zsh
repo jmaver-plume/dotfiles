ca6c834450b694b3d615e5f581963b9e6e7e4569
Jaka Maver <jmaver@plume.com>
2023-08-17 08:57:23 +0200

    feat: Add reset_integration_branch function

--- oh-my-zsh/custom/git.zsh
+++ oh-my-zsh/custom/git.zsh
@@ -1,13 +1,18 @@
 git config --global --add --bool push.autoSetupRemote true
 
-
-# Configure git to use diff-so-fancy for git diff 
+# Configure git to use diff-so-fancy for git diff
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 git config --global interactive.diffFilter "diff-so-fancy --patch"
 
-
-# Places the checked out branch name to the pasteboard
+# Usage: copy_branch_name
 copy_branch() {
   git branch | grep \* | cut -d ' ' -f2 | tee >(pbcopy)
+}
+
+# Usage: reset_integration_branch
+reset_integration_branch() {
+  git fetch -a || { echo "Fetch failed, aborting"; return 1; }
+  git checkout integration || { echo "Checkout failed, aborting"; return 1; }
+  git reset --hard origin/integration || { echo "Reset failed, aborting"; return 1; }
 }
 
