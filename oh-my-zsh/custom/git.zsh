9f35faac6ef61f993ff2b28ab8a6f63799fb7485
Jaka Maver <jmaver@plume.com>
2024-10-28 08:33:55 +0100

    feat(git): add custom functions

--- oh-my-zsh/custom/git.zsh
+++ oh-my-zsh/custom/git.zsh
@@ -8,3 +8,55 @@ git config --global --add --bool push.autoSetupRemote true
 # Configure git to use diff-so-fancy for better 'git diff' visualization.
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 git config --global interactive.diffFilter "diff-so-fancy --patch"
+
+
+# -----------------------
+# Custom Git Functions
+# -----------------------
+
+
+# gbranch
+# Copies the current git branch name to the clipboard.
+# Usage: gbranch
+gbranch() {
+  git branch | grep \* | cut -d ' ' -f2 | tee >(pbcopy)
+}
+
+# greset
+# Resets the local 'integration' branch to match the remote 'origin/integration' branch.
+# Usage: greset
+greset() {
+  git fetch -a || { echo "Fetch failed, aborting"; return 1; }
+  git checkout integration || { echo "Checkout failed, aborting"; return 1; }
+  git reset --hard origin/integration || { echo "Reset failed, aborting"; return 1; }
+}
+
+# grename
+# Renames the current Git branch to a new name and updates the remote branch.
+# Usage: grename new_branch_name
+grename() {
+    # Check if desired branch name is provided
+    if [ "$#" -ne 1 ]; then
+        echo "Error: New branch name not specified."
+        echo "Usage: grename new_branch_name"
+        return 1
+    fi
+
+    # Get the current branch name
+    local current_branch
+    current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+    # Check if current branch is retrieved successfully
+    if [ -z "$current_branch" ]; then
+        echo "Error: Could not determine the current branch."
+        return 1
+    fi
+
+    # Rename the current branch
+    git branch -m "$1"
+
+    # Change the remote tracking branch
+    git push origin -u "$1"
+
+    echo "Branch renamed from '$current_branch' to '$1' and remote updated."
+}
